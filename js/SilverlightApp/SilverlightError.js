bd490ce89a20b36bfdeefadb58203e35a6cde837
Adrian Flannery <aflanry@gmail.com>
2015-06-06 10:56:52 -0500

    Small fix to dropdown menu style

--- js/SilverlightApp/SilverlightError.js
+++ js/SilverlightApp/SilverlightError.js
@@ -1,36 +1,34 @@
-<script type="text/javascript">
-        function onSilverlightError(sender, args) {
-            var appSource = "";
-            if (sender != null && sender != 0) {
-              appSource = sender.getHost().Source;
-            }
-            
-            var errorType = args.ErrorType;
-            var iErrorCode = args.ErrorCode;
+function onSilverlightError(sender, args) {
+    var appSource = "";
+    if (sender != null && sender != 0) {
+      appSource = sender.getHost().Source;
+    }
+    
+    var errorType = args.ErrorType;
+    var iErrorCode = args.ErrorCode;
 
-            if (errorType == "ImageError" || errorType == "MediaError") {
-              return;
-            }
+    if (errorType == "ImageError" || errorType == "MediaError") {
+      return;
+    }
 
-            var errMsg = "Unhandled Error in Silverlight Application " +  appSource + "\n" ;
+    var errMsg = "Unhandled Error in Silverlight Application " +  appSource + "\n" ;
 
-            errMsg += "Code: "+ iErrorCode + "    \n";
-            errMsg += "Category: " + errorType + "       \n";
-            errMsg += "Message: " + args.ErrorMessage + "     \n";
+    errMsg += "Code: "+ iErrorCode + "    \n";
+    errMsg += "Category: " + errorType + "       \n";
+    errMsg += "Message: " + args.ErrorMessage + "     \n";
 
-            if (errorType == "ParserError") {
-                errMsg += "File: " + args.xamlFile + "     \n";
-                errMsg += "Line: " + args.lineNumber + "     \n";
-                errMsg += "Position: " + args.charPosition + "     \n";
-            }
-            else if (errorType == "RuntimeError") {           
-                if (args.lineNumber != 0) {
-                    errMsg += "Line: " + args.lineNumber + "     \n";
-                    errMsg += "Position: " +  args.charPosition + "     \n";
-                }
-                errMsg += "MethodName: " + args.methodName + "     \n";
-            }
-
-            throw new Error(errMsg);
+    if (errorType == "ParserError") {
+        errMsg += "File: " + args.xamlFile + "     \n";
+        errMsg += "Line: " + args.lineNumber + "     \n";
+        errMsg += "Position: " + args.charPosition + "     \n";
+    }
+    else if (errorType == "RuntimeError") {           
+        if (args.lineNumber != 0) {
+            errMsg += "Line: " + args.lineNumber + "     \n";
+            errMsg += "Position: " +  args.charPosition + "     \n";
         }
-    </script>
+        errMsg += "MethodName: " + args.methodName + "     \n";
+    }
+
+    throw new Error(errMsg);
+}
