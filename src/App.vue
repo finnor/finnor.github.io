1ca3691a73a849172c55763583651e07340ad8a3
Adrian Flannery <aflanry@gmail.com>
2020-12-26 23:21:39 -0600

    Added landing section to home page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-brand-primary">
     <app-nav/>
     <router-view/>
     <app-footer/>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="row">
+  <footer>
     <div class="offset-sm-3 col-sm-9 text-left">Adrian Flannery</div>
   </footer>
 </template>
