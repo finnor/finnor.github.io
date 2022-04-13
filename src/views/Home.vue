fa6337fc37fc5539eef1e8aa7c0d3385ea1e0834
Adrian Flannery <aflanry@gmail.com>
2022-04-12 21:04:44 -0500

    Updated resume 2022

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -67,7 +67,7 @@
                     'value': 15,
                   },
                   {
-                    'label':'Exercise',
+                    'label':'Hiking',
                     'value': 20,
                   }
                 ]
@@ -131,57 +131,22 @@
                 <router-link to="/DNAAlignmentXY" class="float-right btn btn-brand-secondary btn-sm"><i class="fa fa-angle-double-right fa-lg"></i></router-link>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div class="card border border-brand-secondary mb-5">
-        <h1 class="card-header bg-white text-center"><i class="fa fa-folder-open"></i> Other Projects</h1>
-          <div class="card-body">
-            <div class="row">
-              <div class="col-md-4">
-                <a href="http://apps.cytoscape.org/apps/wkshelldecomposition" target="_blank" rel=”noopener”><img class="img-fluid d-block mx-auto" src="http://apps.cytoscape.org/media/wkshelldecomposition/screenshots/Example%20Network%20Layout.png" alt="wk Shell Decomposition"></a>
-              </div>
-              <div class="col-md-8">
-                <h2><a href="http://apps.cytoscape.org/apps/wkshelldecomposition" target="_blank" rel=”noopener”>wk Shell Decomposition</a></h2>
-                <p class="lead text-muted">
-                  wk-shell-decomposition is a Cytoscape 3 app that performs a k-shell decomposition on a network using a weighted algorithm.
-                  k-shell decomposition is a method that ranks the most important nodes in a network and partitions them into shells based on that rank.
-                  The app also adjusts the layout of the network to packed concentric rings sorted by k-shell and applies a gradient to the color of the nodes by k-shell.
-                </p>
-                <p class="text-lg">
-                  &bull; <a href="https://github.com/finnor/wk-shell-decomposition" target="_blank" rel="noopener">git</a> 
-                </p>
-                <a href="http://apps.cytoscape.org/apps/wkshelldecomposition" target="_blank" rel=”noopener” class="float-right btn btn-brand-secondary btn-sm"><i class="fa fa-angle-double-right fa-lg"></i></a>
-              </div>
+          <hr>
+          <div class="row">
+            <div class="col-md-4">
+              <a href="http://apps.cytoscape.org/apps/wkshelldecomposition" target="_blank" rel=”noopener”><img class="img-fluid d-block mx-auto" src="http://apps.cytoscape.org/media/wkshelldecomposition/screenshots/Example%20Network%20Layout.png" alt="wk Shell Decomposition"></a>
             </div>
-            <hr>
-            <div class="row">
-              <div class="offset-md-4 col-md-8">
-                <h2><a href="https://github.com/finnor/big-spoon-alerts" target="_blank" rel=”noopener”>Big Spoon Alerts</a></h2>
-                <p class="lead text-muted">
-                  Do you like ice cream?
-                  Big Spoon Alerts scrapes the web page of a local ice cream shop for its menu, then compares the it to the previous iteration of the menu.
-                  Upon detecting changes, it generates an email notification to report the added and dropped flavors to the configured email address.
-                </p>
-                <p class="text-lg">
-                  &bull; <a href="https://github.com/finnor/big-spoon-alerts" target="_blank" rel="noopener">git</a> 
-                </p>
-                <a href="https://github.com/finnor/big-spoon-alerts" target="_blank" rel=”noopener” class="float-right btn btn-brand-secondary btn-sm"><i class="fa fa-angle-double-right fa-lg"></i></a>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-            <div class="offset-md-4 col-md-8">
-              <h2><a href="https://github.com/finnor/reddit-submission-diff" target="_blank" rel=”noopener”>Reddit Submission Diff</a></h2>
+            <div class="col-md-8">
+              <h2><a href="http://apps.cytoscape.org/apps/wkshelldecomposition" target="_blank" rel=”noopener”>wk Shell Decomposition</a></h2>
               <p class="lead text-muted">
-                Are there any posts that receive regular updates on Reddit you follow, e.g. &quot;Best Of&quot; lists, FAQs, etc?
-                Reddit Submission Diff uses the Reddit API to read the content of a designated post, then compares the post to the previous content of the post.
-                It will then report a diff of the post's content to the configured email address.
+                wk-shell-decomposition is a Cytoscape 3 app that performs a k-shell decomposition on a network using a weighted algorithm.
+                k-shell decomposition is a method that ranks the most important nodes in a network and partitions them into shells based on that rank.
+                The app also adjusts the layout of the network to packed concentric rings sorted by k-shell and applies a gradient to the color of the nodes by k-shell.
               </p>
               <p class="text-lg">
-                &bull; <a href="https://github.com/finnor/reddit-submission-diff" target="_blank" rel="noopener">git</a> 
+                &bull; <a href="https://github.com/finnor/wk-shell-decomposition" target="_blank" rel="noopener">git</a> 
               </p>
-              <a href="https://github.com/finnor/reddit-submission-diff" target="_blank" rel=”noopener” class="float-right btn btn-brand-secondary btn-sm"><i class="fa fa-angle-double-right fa-lg"></i></a>
+              <a href="http://apps.cytoscape.org/apps/wkshelldecomposition" target="_blank" rel=”noopener” class="float-right btn btn-brand-secondary btn-sm"><i class="fa fa-angle-double-right fa-lg"></i></a>
             </div>
           </div>
         </div>
@@ -206,19 +171,25 @@
             <div class="col-md-8">
               <ul>
                 <li>
-                  Assist in the design, development and implementation of new computer programs including enterprise-level web applications and systems as a project section lead.
+                  Designed, implemented, and maintained a data pipeline development framework devised to promote rapid development of ETL pipelines when extracting from the same source and/or loading to the same target.
                 </li>
                 <li>
-                  Utilize modern HTML5+Javascript centered web computing, software development methodologies and object-oriented development methodologies to develop software applications and database systems (relational and NoSQL).
+                  Developed data pipelines to ingest and load multi-omic results from vendor and open source formats to multiple data analysis applications, empowering researchers with clean, accessible data.
                 </li>
                 <li>
-                  Provide support using automated testing processes and version control systems.
+                  Developed bioinformatics pipelines to analyze DNASeq data for small variant, CNV, and SV calling.
                 </li>
                 <li>
-                  Provide support for the analysis, design, estimation, development, documentation, testing, and implementation of new applications.
+                  Developed a data pipeline and corresponding data analysis application to load millions of EMR records and subsequently derive insights on blood culture contamination and antimicrobial susceptibility patterns.
                 </li>
                 <li>
-                  Provide maintenance and support for programs within existing systems.
+                  Designed, implemented, and maintained a web application to facilitate NGS analysis, interpretation, and reporting.
+                </li>
+                <li>
+                  Defined test plans and implemented unit, integration, system and regression testing to verify and validate systems.
+                </li>
+                <li>
+                  Regularly present status and findings to stakeholders.
                 </li>
               </ul>
             </div>
@@ -235,7 +206,7 @@
             <div class="col-md-8">
               <ul>
                 <li>
-                  Assist in the design, development and implementation of new computer programs including enterprise-level web applications and systems.
+                  Assist in the design, development and implementation of new computer programs including enterprise-level web applications and systems as a project section lead.
                 </li>
                 <li>
                   Utilize modern HTML5+Javascript centered web computing, software development methodologies and object-oriented development methodologies to develop software applications and database systems (relational and NoSQL).
@@ -265,27 +236,6 @@
       <div class="card border border-brand-secondary mb-5">
         <h1 class="card-header bg-white text-center"><i class="fa fa-graduation-cap"></i> Education</h1>
         <div class="card-body">
-          <div class="row border p-3 m-1">
-            <h4 class="col-md-12">BS - Biology: Concentration in Molecular Biology</h4>
-            <div class="col-md-8">
-              <ul>
-                <li>Systems Biology</li>
-                <li>Molecular Genetics</li>
-                <li>Genetics</li>
-                <li>Epigenetics</li>
-                <li>DNA Technology</li>
-                <li>Cell Biology</li>
-                <li>Microbiology</li>
-                <li>Biochemistry</li>
-              </ul>
-            </div>
-            <div class="col-md-4">
-              <div class="p-2 border-left border-wide">
-                <p>Spring 2016 - Summer 2019</p>
-                <p>University of Alabama at Birmingham</p>
-              </div>
-            </div>
-          </div>
           <div class="row border p-3 m-1">
             <h4 class="col-md-12">MS - Software Engineering</h4>
             <div class="col-md-8">
@@ -323,6 +273,27 @@
             <div class="col-md-4">
               <div class="p-2 border-left border-wide">
                 <p>Fall 2005 - Spring 2011</p>
+                <p>University of Alabama at Birmingham</p>
+              </div>
+            </div>
+          </div>
+          <div class="row border p-3 m-1">
+            <h4 class="col-md-12">BS - Biology: Concentration in Molecular Biology</h4>
+            <div class="col-md-8">
+              <ul>
+                <li>Systems Biology</li>
+                <li>Molecular Genetics</li>
+                <li>Genetics</li>
+                <li>Epigenetics</li>
+                <li>DNA Technology</li>
+                <li>Cell Biology</li>
+                <li>Microbiology</li>
+                <li>Biochemistry</li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <div class="p-2 border-left border-wide">
+                <p>Spring 2016 - Summer 2019</p>
                 <p>University of Alabama at Birmingham</p>
               </div>
             </div>
